5338ea35482e513a2fe00cb20545ee5d279b766b
andrearenso <dedearenso@gmail.com>
2025-12-06 22:09:43 -0300

    Adicioneu um texto

--- app/page.tsx
+++ app/page.tsx
@@ -32,6 +32,10 @@ export default function Home() {
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
           <Hero />
+          <div className="flex flex-col gap-4 items-center w-full">
+            <h2 className="font-bold text-2xl">Aqui está nossas Mudanças</h2>
+            {/* Adicione mais detalhes sobre mudanças aqui, se desejar */}
+          </div>
           <main className="flex-1 flex flex-col gap-6 px-4">
             <h2 className="font-medium text-xl mb-4">Next steps</h2>
             {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
